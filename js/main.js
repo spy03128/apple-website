bc1dd716243b60b77309c59c0d21e457f0d8d989
spy03128 <spy03128@naver.com>
2021-03-29 02:14:46 +0900

    스크롤 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
   let yOffset = 0; //window.pageYoffset 대신 사용할 변수
   let prevScrollHeight = 0; //현재 스크롤 위치 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
   let currentScene = 0; //현재 활성화된(보고있는) scene(scrollsection)
+  let enterNewScene = false; //새로운 씬이 시작되는 순간 true
 
   const sceneInfo = [
     {
@@ -10,7 +11,16 @@
       heightNum: 5, //브라우저높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
+        //dom 객체 요소
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+      values: {
+        //값에 해당하는 요소
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -67,7 +77,38 @@
     document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    //현재 씬에서 스크롤된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    return rv;
+  }
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
   function scrollLoop() {
+    enterNewScene = false;
     prevScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
@@ -75,17 +116,25 @@
 
     //현재 스크롤값이 이전 섹션 스크롤 값의 합 + 현재 섹션 스크롤값 보다 크면
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++; //현재 섹션 번호를 1 증가
+      //setAttribute는 선택한 요소의 속상값을 정하는 것
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
     //현재 스크롤값이 이전 섹션 스크롤 값의 합보다 작으면
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
+
       if (currentScene === 0) return;
       currentScene--; //현재 섹션 번호를 1 감소
+      //setAttribute는 선택한 요소의 속상값을 정하는 것
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
-    //setAttribute는 선택한 요소의 속상값을 정하는 것
-    document.body.setAttribute("id", `show-scene-${currentScene}`);
+    if (enterNewScene) return;
+
+    playAnimation();
   }
 
   window.addEventListener("resize", setLayout); //윈도우 창을 줄였을때 레이아웃 재설정
