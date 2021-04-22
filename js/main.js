ab039a7c5c8dd5f39b93f01813120743a0e2ef80
spy03128 <spy03128@naver.com>
2021-04-23 01:46:33 +0900

    image blending

--- js/main.js
+++ js/main.js
@@ -110,6 +110,7 @@
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
         rectStartY: 0,
+        blendHeight: [0, 0, { start: 0, end: 0 }],
       },
     },
   ];
@@ -534,6 +535,27 @@
           objs.canvas.classList.remove("sticky");
         } else {
           step = 2;
+          //이미지 블랜드 처리
+          //blendHeight: [0, 0, {start:0, end:0}]
+          values.blendHeight[0] = 0;
+          values.blendHeight[1] = objs.canvas.height;
+          values.blendHeight[2].start = values.rect1X[2].end;
+          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
+          // objs.context.drawImage(img, x, y, width, height); 형태
+          objs.context.drawImage(
+            objs.images[1],
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight,
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight
+          );
+
           objs.canvas.classList.add("sticky");
           objs.canvas.style.top = `${
             -(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2
